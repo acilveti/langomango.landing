ebe786a0b6fdda7c0692e983e0a7b40dc13b08fb
acilveti <ander.cilveti.alvarez@gmail.com>
2025-04-27 16:20:47 +0200

    fixing bug

--- components/Accordion.tsx
+++ components/Accordion.tsx
@@ -21,17 +21,7 @@ export default function Accordion({ title, isOpen, children }: PropsWithChildren
     <AccordionWrapper onClick={() => setHasCollapsed((prev) => !prev)}>
       <TitleWrapper>
         <Title>{title}</Title>
-        <Icon isActive={isActive}>
-          <svg
-            viewBox="0 0 24 24"
-            focusable="false"
-            className="chakra-icon chakra-accordion__icon css-j2ph2z"
-            aria-hidden="true"
-            preserveAspectRatio="none"
-          >
-            <path fill="currentColor" d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z"></path>
-          </svg>
-        </Icon>
+        
       </TitleWrapper>
       <Collapse isOpen={isActive} duration={300}>
         <Description>
