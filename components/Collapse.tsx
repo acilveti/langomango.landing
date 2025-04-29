0244be8b3db25b37da7817f78f21bf8dab86cdfb
acilveti <ander.cilveti.alvarez@gmail.com>
2025-04-29 10:05:20 +0200

    fix vercel

--- components/Collapse.tsx
+++ components/Collapse.tsx
@@ -23,6 +23,7 @@ const Collapse = forwardRef<HTMLDivElement, PropsWithChildren<CollapseProps>>(
       easing = 'ease',
       startingHeight = 0,
       endingHeight = 'auto',
+      children,
       ...rest
     },
     ref,
@@ -34,16 +35,21 @@ const Collapse = forwardRef<HTMLDivElement, PropsWithChildren<CollapseProps>>(
         animateOpacity={animateOpacity}
         height={isOpen ? endingHeight : startingHeight}
         applyInlineTransitions={false}
-        {...{ onAnimationStart, onAnimationEnd }}
+        onAnimationStart={onAnimationStart}
+        onAnimationEnd={onAnimationEnd}
         style={{
           transition: 'height .3s ease,opacity .3s ease-in-out,transform .3s ease-in-out',
           backfaceVisibility: 'hidden',
         }}
       >
-        <div ref={ref} {...rest} />
+        <div ref={ref} {...rest}>
+          {children}
+        </div>
       </AnimateHeight>
     );
   },
 );
+
+Collapse.displayName = 'Collapse';
 
 export default Collapse;
