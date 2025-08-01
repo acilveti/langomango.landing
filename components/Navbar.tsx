7e518e9d0188727d9a90fd74a4c66256080c893e
acilveti <ander.cilveti.alvarez@gmail.com>
2025-08-01 13:15:31 +0200

    fix

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,8 +5,8 @@ import styled from 'styled-components';
 import { ScrollPositionEffectProps, useScrollPosition } from 'hooks/useScrollPosition';
 import { NavItems, SingleNavItem } from 'types';
 import Container from './Container';
-import Logo from './Logo2Lines';
 import ExpandingButton from './ExpandingButton'; 
+import Logo from './Logo2Lines';
  
 
 type NavbarProps = { items: NavItems };
