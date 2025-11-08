87661c475e2bfeb14159ffd48cd406b94e06e5eb
acilveti-think <ander.cilveti.alvarez@gmail.com>
2025-11-08 12:02:38 +0100

    landing page

--- views/SimplePage/Hero.tsx
+++ views/SimplePage/Hero.tsx
@@ -117,7 +117,7 @@ export default function Hero() {
             <DeviceFrame scrollProgress={scrollProgress}>
               <DeviceScreen>
                 <StyledIframe
-                  src="https://ebook-beta.langomango.com/demo_user_1762019550748_9416/reader?chapter=2"
+                  src="https://ebook-beta.langomango.com/demo_user_1762019550748_9416/landing-reader?chapter=2"
                   title="LangoMango Reader Demo"
                 />
               </DeviceScreen>
